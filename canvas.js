cef73ce329700d5478513ec212fad275c3fd3a21
よや <yoya@awm.jp>
2012-01-04 01:10:27 +0900

    add drawFill & drawLines

--- canvas.js
+++ canvas.js
@@ -1,9 +1,49 @@
 var SWFCanvas = function (canvas_id) {
     var canvas = document.getElementById(canvas_id);
+    this.canvas = canvas;
     var ctx = canvas.getContext('2d');
-  /* 四角を描く */  ctx.beginPath();  ctx.moveTo(20, 20);  ctx.lineTo(120, 20);  ctx.lineTo(120, 120);  ctx.lineTo(20, 120);  ctx.closePath();  ctx.stroke();
     this.setBackgroundColor = function(rgb) {
 	console.debug('SWFCanvas::setBackgroundColor('+rgb+')');
-	canvas.style.backgroundColor = 'rgb('+rgb.Red+','+rgb.Green+','+rgb.Blue+')';
+	canvas.style.backgroundColor = rgb.toStringCSS();
+    }
+    // http://www.html5.jp/canvas/how5.html
+    this.drawRadialGradient = function(x, y, w, colorStops) {
+        ctx.beginPath();
+        var grad  = ctx.createRadialGradient(x, y, 0, x, y, w);
+        if (colorStops) {
+            for (var i = 0, n = colorStops.length; i < n ; i++) {
+                var ratio = colorStops[i][0], color = colorStops[i][1];
+                grad.addColorStop(ratio, color);
+            }
+        }
+        ctx.fillStyle = grad;
+        ctx.rect(x-w, y-w, x+w, y+w);
+        ctx.fill();
+    }
+    // ex) drawFill([[10, 10],[100, 10], [100, 100], [10, 100]], 'yellow', 3);
+    this.drawFill = function(edges, color) {
+        ctx.fillStyle = color;
+        ctx.beginPath();
+        ctx.moveTo(edges[0][0], edges[0][1]);
+        for (var i = 1, n = edges.length ; i < n ; i++) {
+            console.debug(edges[i][0]+"-"+edges[i][1])
+            ctx.lineTo(edges[i][0], edges[i][1]);
+        }
+        ctx.closePath();
+        ctx.fill();
+    }
+    // https://developer.mozilla.org/ja/Canvas_tutorial/Applying_styles_and_colors
+    // ex) drawLines([[10, 10],[100, 10], [100, 100], [10, 100]], 'yellow', 3);
+    this.drawLines = function(edges, color, width) {
+        ctx.lineWidth = width?width:0;
+        ctx.strokeStyle = color;
+        ctx.beginPath();
+        ctx.moveTo(edges[0][0], edges[0][1]);
+        for (var i = 1, n = edges.length ; i < n ; i++) {
+            console.debug(edges[i][0]+"-"+edges[i][1])
+            ctx.lineTo(edges[i][0], edges[i][1]);
+        }
+        ctx.closePath();
+        ctx.stroke();
     }
 }
